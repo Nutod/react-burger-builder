0613d092f8606539070bc9bbe9d900ae457dde3b
Nutod <alukodotun@gmail.com>
2018-09-18 14:21:44 +0100

    Burger Ingredient Seed2 added

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -67,9 +67,30 @@ const Seeds1 = styled.div`
 	}
 `;
 
-// const Seeds2 = styled.div`
+const Seeds2 = styled.div`
+	width: 10%;
+	height: 15%;
+	position: absolute;
+	background-color: white;
+	left: 64%;
+	top: 50%;
+	border-radius: 40%;
+	transform: rotate(10deg);
+	box-shadow: inset -3px 0 #c9c9c9;
 
-// `
+	&::before {
+		content: "";
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		background-color: white;
+		left: 150%;
+		top: -130%;
+		border-radius: 40%;
+		transform: rotate(90deg);
+		box-shadow: inset 1px 3px #c9c9c9;
+	}
+`;
 
 // TODO: Possible refactor to use Functional Component
 export default class BurgerIngredient extends Component {
