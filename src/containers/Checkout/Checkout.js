24bd76087120590594fda922000fb8d4b60d87a7
Nutod <alukodotun@gmail.com>
2018-09-25 15:43:52 +0100

    Checkout functionality wired up

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,11 +1,16 @@
 import React, { Component, Fragment } from "react";
+import Burger from "../../components/Burger/Burger";
 
 export default class extends Component {
+	componentDidMount = () => {
+		console.log(this.props);
+	};
+
 	render() {
 		return (
 			<Fragment>
-				<h1>Here's the Order</h1>
-				<p>Burger Component</p>
+				<h3 style={{ textAlign: "center" }}>Here's the Order</h3>
+				<Burger ingredients={this.props.location.state.ingredients} />
 			</Fragment>
 		);
 	}
