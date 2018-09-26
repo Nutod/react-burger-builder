f5327d982b502bccf6f1d319159be397772ea184
Nutod <alukodotun@gmail.com>
2018-09-26 12:09:48 +0100

    Added the Form Submission Handler

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { ButtonSuccess } from "../../../components/Burger/OrderSummary/OrderSummary";
 
 const ContactDataWrapper = styled.div`
-  margin: 20px auto;
-  widthL 80%;
-  text-align: center;
+	margin: 20px auto;
+	width: 80%;
+	text-align: center;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+	border: 1px solid #eee;
+	box-sizing: border-box;
 
-  @media (min-width: 600px) {
-    width: 500px
-  }
+	@media (min-width: 600px) {
+		width: 500px;
+	}
+`;
+
+const Input = styled.input`
+	display: block;
+	width: 40vw;
+	font-family: inherit;
+	padding: 5px;
+	margin: 5px auto;
 `;
 
 class ContactData extends Component {
@@ -21,15 +33,21 @@ class ContactData extends Component {
 		}
 	};
 
+	orderHandler = event => {
+		event.preventDefault();
+		alert("Someone clicked something...");
+	};
+
 	render() {
 		return (
 			<ContactDataWrapper>
 				<h4>Enter Contact Data</h4>
-				<form action="" method="post">
-					<input type="text" name="name" placeholder="Your Name" />
-					<input type="email" name="email" placeholder="Your Email" />
-					<input type="text" name="street" placeholder="Your Street" />
-					<input type="text" name="postalcode" placeholder="Your Postal Code" />
+				<form onSubmit={this.orderHandler}>
+					<Input type="text" name="name" placeholder="Your Name" />
+					<Input type="email" name="email" placeholder="Your Email" />
+					<Input type="text" name="street" placeholder="Your Street" />
+					<Input type="text" name="postalcode" placeholder="Your Postal Code" />
+					<ButtonSuccess>ORDER</ButtonSuccess>
 				</form>
 			</ContactDataWrapper>
 		);
