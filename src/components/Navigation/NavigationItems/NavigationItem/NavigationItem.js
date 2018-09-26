dad1e8dab9db013167da4bbc81868f1a8cc47fb3
Nutod <alukodotun@gmail.com>
2018-09-26 09:06:53 +0100

    Adjusted the NavigationLink

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -18,11 +18,38 @@ const NavigationListItem = styled.li`
 	}
 
 	& > * {
-		color: #8f5c2c;
+		color: #fff;
 		text-decoration: none;
 		width: 100 %;
 		box-sizing: border-box;
 		display: block;
+
+		&:hover {
+			color: #40a4c8;
+		}
+
+		&:active {
+			color: #40a4c8;
+		}
+
+		@media (min-width: 500px) {
+			color: white;
+			height: 100 %;
+			padding: 16px 10px;
+			border-bottom: 4px solid transparent;
+
+			&:hover {
+				background-color: #8f5c2c;
+				border-bottom: 4px solid #40a4c8;
+				color: white;
+			}
+
+			&:active {
+				background-color: #8f5c2c;
+				border-bottom: 4px solid #40a4c8;
+				color: white;
+			}
+		}
 	}
 `;
 
