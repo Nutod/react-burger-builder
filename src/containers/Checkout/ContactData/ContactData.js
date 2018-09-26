7b71674325866609dad2cc8611701badcf1fbe17
Nutod <alukodotun@gmail.com>
2018-09-26 11:52:47 +0100

    Added styles for the ContactData

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,39 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
-class ContactData extends Component {}
+const ContactDataWrapper = styled.div`
+  margin: 20px auto;
+  widthL 80%;
+  text-align: center;
+
+  @media (min-width: 600px) {
+    width: 500px
+  }
+`;
+
+class ContactData extends Component {
+	state = {
+		name: "",
+		email: "",
+		address: {
+			street: "",
+			postalCode: ""
+		}
+	};
+
+	render() {
+		return (
+			<ContactDataWrapper>
+				<h4>Enter Contact Data</h4>
+				<form action="" method="post">
+					<input type="text" name="name" placeholder="Your Name" />
+					<input type="email" name="email" placeholder="Your Email" />
+					<input type="text" name="street" placeholder="Your Street" />
+					<input type="text" name="postalcode" placeholder="Your Postal Code" />
+				</form>
+			</ContactDataWrapper>
+		);
+	}
+}
 
 export default ContactData;
