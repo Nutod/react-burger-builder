f9c646c02372c5701fc816fdf2333a26a468bbd6
Nutod <alukodotun@gmail.com>
2018-09-29 12:52:46 +0100

    removeIngredient action creator added

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -5,3 +5,8 @@ export const addIngredient = ingredient => ({
 	type: actionTypes.ADD_INGREDIENT,
 	ingredient
 });
+
+export const removeIngredient = ingredient => ({
+	type: actionTypes.REMOVE_INGREDIENT,
+	ingredient
+});
