1883240eb69a8e1650adde6706310fe27bcd7cd1
Nutod <alukodotun@gmail.com>
2018-09-29 12:51:34 +0100

    addIngredient action creator added

---
+++ src/store/actions/burgerBuilder.js
@@ -0,0 +1,7 @@
+import * as actionTypes from "./actionTypes";
+
+// Sychronous action creator dispatched in components
+export const addIngredient = ingredient => ({
+	type: actionTypes.ADD_INGREDIENT,
+	ingredient
+});
