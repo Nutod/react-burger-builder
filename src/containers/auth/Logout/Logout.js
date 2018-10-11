b4712fa1083e4fe07a674fd046e71fd8b39137b7
Nutod <alukodotun@gmail.com>
2018-10-11 10:18:25 +0100

    Logout Component functionality added

--- src/containers/auth/Logout/Logout.js
+++ src/containers/auth/Logout/Logout.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../AuthActions";
 
 class Logout extends Component {
+	componentDidMount = () => {
+		this.props.onLogout();
+	};
+
 	render() {
-		return <div />;
+		return <Redirect to="/" />;
 	}
 }
 
-export default Logout;
+const mapDispatchToProps = dispatch => ({
+	onLogout: logout(dispatch)
+});
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(Logout);
