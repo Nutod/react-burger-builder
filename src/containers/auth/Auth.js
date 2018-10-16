5a829be2cd3afd5ffe77b4880e54e9dcb709f078
Nutod <alukodotun@gmail.com>
2018-10-16 14:13:33 +0100

    Adjusted Auth Component to use Validation function

--- src/containers/auth/Auth.js
+++ src/containers/auth/Auth.js
@@ -10,6 +10,7 @@ import {
 } from "../../components/Burger/OrderSummary/OrderSummary";
 import { authStart, authSuccess, authFail, logout } from "./AuthActions";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import { checkValidity } from "../../shared/validation";
 
 const AuthWrapper = styled.div`
 	margin: 20px auto;
@@ -58,29 +59,6 @@ class Auth extends Component {
 			}
 		},
 		isSignedUp: false
-	};
-
-	checkValidity = (value, rules) => {
-		let isValid = true;
-
-		if (rules.required) {
-			isValid = value.trim() !== "" && isValid;
-		}
-
-		if (rules.minLength) {
-			isValid = value.length >= rules.minLength && isValid;
-		}
-
-		if (rules.maxLength) {
-			isValid = value.length <= rules.maxLength && isValid;
-		}
-
-		if (rules.isEmail) {
-			const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-			isValid = pattern.test(value) && isValid;
-		}
-
-		return isValid;
 	};
 
 	checkExpirationTime = expirationTime => {
@@ -139,7 +117,6 @@ class Auth extends Component {
 				touched: true
 			}
 		};
-
 		this.setState({ controls });
 	};
 
