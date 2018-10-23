6b6a5da261e279173627557324dea55de3f922ea
Nutod <alukodotun@gmail.com>
2018-10-23 12:36:29 +0100

    Dynamic Import for Buttons added to the OrderSummary Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,6 @@
 import React, { Fragment } from "react";
-import styled from "styled-components";
 
-export const Button = styled.button`
-	background-color: transparent;
-	border: none;
-	color: white;
-	outline: none;
-	cursor: pointer;
-	font: inherit;
-	padding: 10px;
-	margin: 10px;
-	font-weight: bold;
-
-	&:first-of-type {
-		margin-left: 0;
-		padding-left: 0;
-	}
-`;
-export const ButtonSuccess = styled(Button)`
-	color: #5c9210;
-
-	&:disabled {
-		color: #ccc;
-		cursor: not-allowed;
-	}
-`;
-
-export const ButtonDanger = styled(Button)`
-	color: #944317;
-`;
+import { ButtonDanger, ButtonSuccess } from "../../Elements/Button";
 
 export default ({ ingredients, continued, cancelled, price }) => {
 	const item = Object.keys(ingredients).map(ingredient => (
